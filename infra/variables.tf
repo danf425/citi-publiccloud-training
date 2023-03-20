9c2c2ad47445e71f277dd2fde38323d348fccc34
Dan F <danf425@gmail.com>
2023-03-20 13:35:22 -0400

    tf s3 bucket

--- infra/variables.tf
+++ infra/variables.tf
@@ -13,13 +13,19 @@ variable "access_key" {
   default = "temp"
 }
 
+variable "session_token" {
+  default = "temp"
+}
+
 variable "secret_key" {
   default = "temp"
 }
 
 
+
+
 variable "bucket_name" {
-  default = "temp"
+  default = "danf-citi-training-1"
 }
 
 #variable "aws_credentials_file" {
